cbbd944cac3a5a7bccc199c8d7bc986da380032d
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-09-11 11:40:16 +0100

    Update main.py
    Added latest entry of rover 22. Table updates incoming data and shows the latest entry using timestamps column.

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ class Rovers(Enum):
     ROVER_19 = 19
     ROVER_20 = 20
     ROVER_21 = 21
+    ROVER_22 = 22
     BASE_100 = 100
     BASE_101 = 101
 
 global latest_entry_19
 global latest_entry_20
 global latest_entry_21
+global latest_entry_22
 global latest_entry_100 
 global latest_entry_101
 
@@ -25,18 +27,21 @@ TRACKER_IN_DOMAIN = "tracker-in/"
 ROVER_19 = "19"
 ROVER_20 = "20"
 ROVER_21 = "21"
+ROVER_22 = "22"
 BASE_100 = "100"
 BASE_101 = "101"
 
 tracker_in_19_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_19)
 tracker_in_20_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_20)
 tracker_in_21_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_21)
+tracker_in_22_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_22)
 tracker_in_100_url = (MARC_SERVER+TRACKER_IN_DOMAIN+BASE_100)
 tracker_in_101_url = (MARC_SERVER+TRACKER_IN_DOMAIN+BASE_101)
 
 page_19_json = requests.get(tracker_in_19_url, params={'load_amount':10, 'offset': 0})
 page_20_json = requests.get(tracker_in_20_url, params={'load_amount':10, 'offset': 0})
 page_21_json = requests.get(tracker_in_21_url, params={'load_amount':10, 'offset': 0})
+page_22_json = requests.get(tracker_in_22_url, params={'load_amount':10, 'offset': 0})
 page_100_json = requests.get(tracker_in_100_url, params={'load_amount':10, 'offset': 0})
 page_101_json = requests.get(tracker_in_101_url, params={'load_amount':10, 'offset': 0})
 # print(page_19_json.headers['Content-Type']) - returns application/json
@@ -44,24 +49,28 @@ page_101_json = requests.get(tracker_in_101_url, params={'load_amount':10, 'offs
 data_rover_19 = page_19_json.json()
 data_rover_20 = page_20_json.json()
 data_rover_21 = page_21_json.json()
+data_rover_22 = page_22_json.json()
 data_base_100 = page_100_json.json()
 data_base_101 = page_101_json.json()
 
 size_of_19 = (len(data_rover_19))
 size_of_20 = (len(data_rover_20))
 size_of_21 = (len(data_rover_21))
+size_of_22 = (len(data_rover_22))
 size_of_100 = (len(data_base_100))
 size_of_101 = (len(data_base_101))
 
-# Get Latest JSON Entry
+''' Get Latest JSON Entry '''
 latest_entry_19 = data_rover_19[-1]
 latest_entry_20 = data_rover_20[-1]
 latest_entry_21 = data_rover_21[-1]
+latest_entry_22 = data_rover_22[-1]
 latest_entry_100 = data_base_100[-1]
 latest_entry_101 = data_base_101[-1]
 
 # TODO: Last update flag and create a timestamp with it
 last_updated = False
+
 # TODO: while True system to run forever whilst the script is running 
 
 
@@ -106,6 +115,18 @@ for node in list(Rovers):
                 print("%s - Up-to-date" % ROVER_21)
                 latest_entry_21 = data_rover_21[-1]
                 # print(latest_entry_21)
+        case 22:
+            # print("Node 21")
+            # print(data_rover_21[-1])
+            if (len(data_rover_22)) > size_of_22:
+                print(size_of_22)
+                print("New Data Entry Obtained on Node: %s" % ROVER_21)
+                latest_entry_22 = data_rover_22[-1]
+                # print(latest_entry_21)
+            else:
+                print("%s - Up-to-date" % ROVER_22)
+                latest_entry_22 = data_rover_22[-1]
+                # print(latest_entry_21)
         case 100:
             # print("Node 100")
             # print(data_base_100[-1])
@@ -133,7 +154,7 @@ for node in list(Rovers):
         case _:
             print("Invalid Node")
 
-list_of_data = list((latest_entry_19,latest_entry_20,latest_entry_21,latest_entry_100,latest_entry_101))
+list_of_data = list((latest_entry_19,latest_entry_20,latest_entry_21,latest_entry_22,latest_entry_100,latest_entry_101))
 
 with open("latest_data.html", "w") as file:
     file.write(json2html.convert(json=list_of_data, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\""))
