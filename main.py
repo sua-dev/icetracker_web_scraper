861fce88c21bc743a2beffde296f34a74ec88f66
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-09-12 21:51:45 +0100

    Adding functions for readability and removing duplicates.

--- main.py
+++ main.py
@@ -31,8 +31,17 @@ ROVER_22 = "22"
 BASE_100 = "100"
 BASE_101 = "101"
 
+global tracker_urls
+
+def create_instance():
+    tracker_urls = []
+    for node in list(Rovers):
+        tracker_urls.append(tracker_url(node.value))
+    return tracker_urls
+        
+
 def tracker_url(node_id):
-    return (MARC_SERVER+TRACKER_IN_DOMAIN+node_id)
+    return (MARC_SERVER+TRACKER_IN_DOMAIN+str(node_id))
 
 tracker_in_19_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_19)
 tracker_in_20_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_20)
@@ -41,6 +50,8 @@ tracker_in_22_url = (MARC_SERVER+TRACKER_IN_DOMAIN+ROVER_22)
 tracker_in_100_url = (MARC_SERVER+TRACKER_IN_DOMAIN+BASE_100)
 tracker_in_101_url = (MARC_SERVER+TRACKER_IN_DOMAIN+BASE_101)
 
+
+# can create a for a loop to instantiate multiple nodes 
 def request_page(tracker_page):
     return requests.get(tracker_page, params={'load_amount':10, 'offset': 0})
 
@@ -88,92 +99,92 @@ last_updated = False
 
 # TODO: while True system to run forever whilst the script is running 
 
+def update_table():
+    for node in list(Rovers):
+        # print(node.value)
+        match node.value:
+            case 19:
+                # print("Node 19")
+                # print(data_rover_19[-1])
+                if (len(data_rover_19)) > size_of_19:
+                    print(size_of_19)
 
-for node in list(Rovers):
-    # print(node.value)
-    match node.value:
-        case 19:
-            # print("Node 19")
-            # print(data_rover_19[-1])
-            if (len(data_rover_19)) > size_of_19:
-                print(size_of_19)
+                    print("New Data Entry Obtained on Node: %s" % ROVER_19)
+                    latest_entry_19 = data_rover_19[-1]
+                    # print(latest_entry_19)
+                else:
+                    print("%s - Up-to-date" % ROVER_19)
+                    latest_entry_19 = data_rover_19[-1]
+                    # print(latest_entry_19)
+            case 20:
+                # print("Node 20")
+                # print(data_rover_20[-1])
+                
+                if (len(data_rover_20)) > size_of_20:
+                    print(size_of_20)
+                    print("New Data Entry Obtained on Node: %s" % ROVER_20)
+                    latest_entry_20 = data_rover_20[-1]
+                    # print(latest_entry_20)
+                else:
+                    print("%s - Up-to-date" % ROVER_20)
+                    latest_entry_20 = data_rover_20[-1]
+                    # print(latest_entry_20)
+            case 21:
+                # print("Node 21")
+                # print(data_rover_21[-1])
+                if (len(data_rover_21)) > size_of_21:
+                    print(size_of_21)
+                    print("New Data Entry Obtained on Node: %s" % ROVER_21)
+                    latest_entry_21 = data_rover_21[-1]
+                    # print(latest_entry_21)
+                else:
+                    print("%s - Up-to-date" % ROVER_21)
+                    latest_entry_21 = data_rover_21[-1]
+                    # print(latest_entry_21)
+            case 22:
+                # print("Node 21")
+                # print(data_rover_21[-1])
+                if (len(data_rover_22)) > size_of_22:
+                    print(size_of_22)
+                    print("New Data Entry Obtained on Node: %s" % ROVER_21)
+                    latest_entry_22 = data_rover_22[-1]
+                    # print(latest_entry_21)
+                else:
+                    print("%s - Up-to-date" % ROVER_22)
+                    latest_entry_22 = data_rover_22[-1]
+                    # print(latest_entry_21)
+            case 100:
+                # print("Node 100")
+                # print(data_base_100[-1])
+                if (len(data_base_100)) > size_of_100:
+                    print(size_of_100)
+                    print("New Data Entry Obtained on Node: %s" % BASE_100)
+                    latest_entry_100 = data_base_100[-1]
+                    # print(latest_entry_100)
+                else:
+                    print("%s - Up-to-date" % BASE_100)
+                    latest_entry_100 = data_base_100[-1]
+                    # print(latest_entry_100)
+            case 101:
+                # print("Node 101")
+                # print(data_base_101[-1])
+                if (len(data_base_101)) > size_of_101:
+                    print(size_of_101)
+                    print("New Data Entry Obtained on Node: %s" % BASE_101)
+                    latest_entry_101 = data_base_101[-1]
+                    # print(latest_entry_101)
+                else:
+                    print("%s - Up-to-date" % BASE_101)
+                    latest_entry_101 = data_base_101[-1]
+                    # print(latest_entry_101)
+            case _:
+                print("Invalid Node")
 
-                print("New Data Entry Obtained on Node: %s" % ROVER_19)
-                latest_entry_19 = data_rover_19[-1]
-                # print(latest_entry_19)
-            else:
-                print("%s - Up-to-date" % ROVER_19)
-                latest_entry_19 = data_rover_19[-1]
-                # print(latest_entry_19)
-        case 20:
-            # print("Node 20")
-            # print(data_rover_20[-1])
-            
-            if (len(data_rover_20)) > size_of_20:
-                print(size_of_20)
-                print("New Data Entry Obtained on Node: %s" % ROVER_20)
-                latest_entry_20 = data_rover_20[-1]
-                # print(latest_entry_20)
-            else:
-                print("%s - Up-to-date" % ROVER_20)
-                latest_entry_20 = data_rover_20[-1]
-                # print(latest_entry_20)
-        case 21:
-            # print("Node 21")
-            # print(data_rover_21[-1])
-            if (len(data_rover_21)) > size_of_21:
-                print(size_of_21)
-                print("New Data Entry Obtained on Node: %s" % ROVER_21)
-                latest_entry_21 = data_rover_21[-1]
-                # print(latest_entry_21)
-            else:
-                print("%s - Up-to-date" % ROVER_21)
-                latest_entry_21 = data_rover_21[-1]
-                # print(latest_entry_21)
-        case 22:
-            # print("Node 21")
-            # print(data_rover_21[-1])
-            if (len(data_rover_22)) > size_of_22:
-                print(size_of_22)
-                print("New Data Entry Obtained on Node: %s" % ROVER_21)
-                latest_entry_22 = data_rover_22[-1]
-                # print(latest_entry_21)
-            else:
-                print("%s - Up-to-date" % ROVER_22)
-                latest_entry_22 = data_rover_22[-1]
-                # print(latest_entry_21)
-        case 100:
-            # print("Node 100")
-            # print(data_base_100[-1])
-            if (len(data_base_100)) > size_of_100:
-                print(size_of_100)
-                print("New Data Entry Obtained on Node: %s" % BASE_100)
-                latest_entry_100 = data_base_100[-1]
-                # print(latest_entry_100)
-            else:
-                print("%s - Up-to-date" % BASE_100)
-                latest_entry_100 = data_base_100[-1]
-                # print(latest_entry_100)
-        case 101:
-            # print("Node 101")
-            # print(data_base_101[-1])
-            if (len(data_base_101)) > size_of_101:
-                print(size_of_101)
-                print("New Data Entry Obtained on Node: %s" % BASE_101)
-                latest_entry_101 = data_base_101[-1]
-                # print(latest_entry_101)
-            else:
-                print("%s - Up-to-date" % BASE_101)
-                latest_entry_101 = data_base_101[-1]
-                # print(latest_entry_101)
-        case _:
-            print("Invalid Node")
+    list_of_data = list((latest_entry_19,latest_entry_20,latest_entry_21,latest_entry_22,latest_entry_100,latest_entry_101))
 
-list_of_data = list((latest_entry_19,latest_entry_20,latest_entry_21,latest_entry_22,latest_entry_100,latest_entry_101))
-
-with open("latest_data.html", "w") as file:
-    file.write(json2html.convert(json=list_of_data, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\""))
-print("Done")
+    with open("latest_data.html", "w") as file:
+        file.write(json2html.convert(json=list_of_data, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\""))
+    print("Done")
 # while True:
 #     if (len(data_rover_19)) > size_of_19:
 #         print("New Data Entry Obtained on Node: %s", ROVER_19)
@@ -181,3 +192,10 @@ print("Done")
 #         print(latest_entry_19)
 #         break
 
+def main():
+    print(create_instance())
+    
+
+if __name__ == '__main__':
+    main()
+    
