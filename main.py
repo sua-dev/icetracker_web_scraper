267d343deab6775a547a0c18f5625a279a2c5cbe
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2023-09-12 22:23:01 +0100

    More generic functions to integrate with cleaning up duplicate code.

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ BASE_100 = "100"
 BASE_101 = "101"
 
 global tracker_urls
+global json_urls
 
 def create_instance():
     tracker_urls = []
@@ -51,6 +52,12 @@ tracker_in_100_url = (MARC_SERVER+TRACKER_IN_DOMAIN+BASE_100)
 tracker_in_101_url = (MARC_SERVER+TRACKER_IN_DOMAIN+BASE_101)
 
 
+def create_json_instance():
+    json_urls = []
+    for url_link in create_instance():
+        json_urls.append(request_page(url_link))
+    return json_urls
+
 # can create a for a loop to instantiate multiple nodes 
 def request_page(tracker_page):
     return requests.get(tracker_page, params={'load_amount':10, 'offset': 0})
@@ -63,8 +70,14 @@ page_100_json = requests.get(tracker_in_100_url, params={'load_amount':10, 'offs
 page_101_json = requests.get(tracker_in_101_url, params={'load_amount':10, 'offset': 0})
 # print(page_19_json.headers['Content-Type']) - returns application/json
 
-def jsonify(data_page):
-    return data_page.json() 
+def jsonify(data_entry):
+    json_data_list = []
+    for element in data_entry:
+        json_data = element.json()
+        json_data_list.append(json_data[-1])
+    return json_data_list
+    # for element in create_json_instance:
+    #     return .json() 
 
 data_rover_19 = page_19_json.json()
 data_rover_20 = page_20_json.json()
@@ -74,6 +87,9 @@ data_base_100 = page_100_json.json()
 data_base_101 = page_101_json.json()
 
 def length_of_data_entries(data_entry):
+    len_list = []
+    for element in data_entry:
+        len_list.append(len(element))
     return len(data_entry)
 
 size_of_19 = (len(data_rover_19))
@@ -87,12 +103,12 @@ size_of_101 = (len(data_base_101))
 def latest_entry(data_entry):
     return data_entry[-1]
 
-latest_entry_19 = data_rover_19[-1]
-latest_entry_20 = data_rover_20[-1]
-latest_entry_21 = data_rover_21[-1]
-latest_entry_22 = data_rover_22[-1]
-latest_entry_100 = data_base_100[-1]
-latest_entry_101 = data_base_101[-1]
+# latest_entry_19 = data_rover_19[-1]
+# latest_entry_20 = data_rover_20[-1]
+# latest_entry_21 = data_rover_21[-1]
+# latest_entry_22 = data_rover_22[-1]
+# latest_entry_100 = data_base_100[-1]
+# latest_entry_101 = data_base_101[-1]
 
 # TODO: Last update flag and create a timestamp with it
 last_updated = False
@@ -193,8 +209,21 @@ def update_table():
 #         break
 
 def main():
-    print(create_instance())
-    
+    # print(create_instance())
+    # print(create_json_instance())
+    # print(data_rover_19[-1])
+    # print()
+    # for i in jsonify(create_json_instance()):
+    #     print(i)
+    # print(jsonify(create_json_instance()))
+    # print(length_of_data_entries(create_json_instance()))
+    latest_entry_19 = create_json_instance()[0]
+    latest_entry_20 = create_json_instance()[1]
+    latest_entry_21 = create_json_instance()[2]
+    latest_entry_22 = create_json_instance()[3]
+    latest_entry_100 = create_json_instance()[4]
+    latest_entry_101 = create_json_instance()[5]
+    update_table()
 
 if __name__ == '__main__':
     main()
